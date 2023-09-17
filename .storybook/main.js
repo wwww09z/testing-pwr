9418bdfa89aca0653ee09fb70865a6f9cd9fbab1
Allen_359857508 <allen359857508@gmail.com>
2023-09-17 16:03:20 +1000

    init commit

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  features: {
+    storyStoreV7: false,
+  },
+};
+export default config;
---
+++ src/components/Requirements/Requirements.jsx
@@ -0,0 +1,1 @@
+export const Requirements = () => <>Hello World! ^ ^</>;
